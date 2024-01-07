91039301444db36ebe7a1106432fddb02f8fb0ec
favour igbokwe <igbokwefc1@gmail.com>
2024-01-07 08:22:30 +0100

    mobile view

---
+++ src/components/Certificate.jsx
@@ -0,0 +1,13 @@
+import { certImg } from "../assets/Landing page"
+
+
+
+const Certificate = () => {
+  return (
+    <div className="ml-[15rem] mt-10">
+      <img className='w-[70vh] h-[70vh]' src={certImg} alt="cert"/>
+    </div>
+  )
+}
+
+export default Certificate
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import {
+  EarthCanvas,
+  // BallCanvas,
+  // ComputersCanvas,
+} from "../canvas";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Salute from "./Certificate";
+import Walking from "./Walking";
+import Sky from "./MarketPlace";
+import WhyAstro from "./WhyAstro";
+
+export { Hero, Navbar, About, Sky, WhyAstro, Salute, EarthCanvas, Walking };
